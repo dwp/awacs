3bb8ccc80f174e2e58457f65a4f24522eadb7b66
dommn <47447454+dommn@users.noreply.github.com>
2021-03-26 13:08:13 +0000

    Reuploading app

---
+++ app/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+router.post('/unhappy-path', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const caseUpdated = req.session.data['case-updated']
+
+  if (caseUpdated === 'no') {
+    res.redirect('/add-a-response')
+  } else {
+    res.redirect('/docs/examples/branching/over-18')
+  }
+})
+
+module.exports = router
