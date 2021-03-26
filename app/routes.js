605729814595efd0e1357e0fe7bc72344e7dfd50
dommn <47447454+dommn@users.noreply.github.com>
2021-03-26 13:05:47 +0000

    Deleted app

--- app/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Add your routes here - above the module.exports line
-
-router.use('/01', require('./views/01/_routes'));
-
-module.exports = router
