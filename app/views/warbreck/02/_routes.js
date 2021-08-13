db48c64b754a91b259783f347991b58b4480d41e
dommn <47447454+dommn@users.noreply.github.com>
2021-08-13 16:20:19 +0100

    Updated Warbreck and Runcorn prototypes with usability testing feedback

--- app/views/warbreck/02/_routes.js
+++ app/views/warbreck/02/_routes.js
@@ -8,10 +8,10 @@ router.post('/search', function (req, res) {
   let searchNino = req.session.data['search']
 
   if (searchNino === 'AA112233B') {
-    res.redirect('search-results')
+    res.redirect('../search-results')
   }
   else if (searchNino === 'AA 11 22 33 B') {
-    res.redirect('search-results')
+    res.redirect('../search-results')
   }
 })
 
